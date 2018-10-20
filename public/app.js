e7ee911b541e9f16c9786b7788506562c52d08d5
Dominik Ilnicki <dominik.ilnicki@gmail.com>
2018-10-20 19:11:45 +0200

    add cool things

--- public/app.js
+++ public/app.js
@@ -1,20 +1,39 @@
 'use strict'
+
+//all staff to create globe
 const globe = new WorldWind.WorldWindow("globe");
 
 globe.addLayer(new WorldWind.BMNGOneImageLayer());
 globe.addLayer(new WorldWind.BMNGLandsatLayer());
-var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
-globe.addLayer(placemarkLayer);
+
 var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
+
+var padsAttributes = new WorldWind.PlacemarkAttributes(null);
+var padsLayer = new WorldWind.RenderableLayer("Pads");
+globe.addLayer(padsLayer);
+globe.addLayer(placemarkLayer);
+
 
 placemarkAttributes.imageOffset = new WorldWind.Offset(
     WorldWind.OFFSET_FRACTION, 0.3,
     WorldWind.OFFSET_FRACTION, 0.0);
 
-placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
+placemarkAttributes.labelAttributes.color = WorldWind.Color.RED;
 placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
     WorldWind.OFFSET_FRACTION, 0.5,
     WorldWind.OFFSET_FRACTION, 1.0);
+
+padsAttributes.imageOffset = new WorldWind.Offset(
+    WorldWind.OFFSET_FRACTION, 0.3,
+    WorldWind.OFFSET_FRACTION, 0.0);
+
+padsAttributes.labelAttributes.color = WorldWind.Color.GREEN;
+padsAttributes.labelAttributes.offset = new WorldWind.Offset(
+    WorldWind.OFFSET_FRACTION, 0.5,
+    WorldWind.OFFSET_FRACTION, 1.0);
+
+
 let layers = [
     // Imagery layers.
     {
@@ -32,65 +51,85 @@ for (var l = 0; l < layers.length; l++) {
     layers[l].layer.enabled = layers[l].enabled;
     globe.addLayer(layers[l].layer);
 }
-// Create a layer to hold the polygons.
-var polygonsLayer = new WorldWind.RenderableLayer();
-polygonsLayer.displayName = "Polygons";
-globe.addLayer(polygonsLayer);
 
-let json = 'https://data.nasa.gov/resource/y77d-th95.geojson';
+
+let json;
 let geoDatas = [];
 
-fetch(json)
-    .then(response => response.json())
-    .then(getData)
+$(function () {
+    fetchData('https://launchlibrary.net/1.4/pad?count=200', processPads);
 
-function getData(data) {
-    data.features.map(item => createPin(item.geometry.coordinates))
+    $("[name=sliderDate]").change(function () {
+        placemarkLayer.removeAllRenderables();
+        var newval = $(this).val();
+        json = 'https://launchlibrary.net/1.4/launch/' + newval + '-01-01' + '/' + newval + '-12-31';
+        console.log(json);
+        fetchData(json, processLaunches);
+    });
+
+    $('input[name="daterange"]').daterangepicker({
+        opens: 'left'
+    }, function (start, end, label) {
+        placemarkLayer.removeAllRenderables();
+        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+        json = 'https://launchlibrary.net/1.4/launch/' + start.format('YYYY-MM-DD') + '/' + end.format('YYYY-MM-DD');
+        console.log(json);
+        fetchData(json, processLaunches);
+    });
+
+
+
+})
+
+
+// fetch data from api
+function fetchData(json, callback) {
+    fetch(json)
+        .then(response => response.json())
+        .then(callback)
 }
 
-function createPin(latlong) {
-    let position = new WorldWind.Position(latlong[0], latlong[1], 100.0);
+
+function processPads(data) {
+    //console.log(data.pads);
+    data.pads.map(pad => createPad(pad))
+}
+
+function processLaunches(data) {
+    globe.goTo(new WorldWind.Location(data.launches[0].location.pads[0].latitude, data.launches[0].location.pads[0].longitude));
+    data.launches.map(launch => createPin(launch))
+}
+
+// create pin
+function createPin(launch) {
+
+    let position = new WorldWind.Position(launch.location.pads[0].latitude, launch.location.pads[0].longitude, 100.0);
 
     let placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
 
-    placemark.label = "Metor\n" +
+    placemark.label = "Start here\n" +
         "Lat " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
         "Lon " + placemark.position.longitude.toPrecision(5).toString();
     placemark.alwaysOnTop = true;
 
     placemarkLayer.addRenderable(placemark);
+    console.log(placemark);
+    new WorldWind.ClickRecognizer(placemark.label, (data) => {
+        console.log(data);
+    })
 }
 
+//create pads
+function createPad(pad) {
+    let lat = parseFloat(pad.latitude);
+    let long = parseFloat(pad.longitude);
 
-// // Define an outer and an inner boundary to make a polygon with a hole.
-// let boundaries = [];
-// boundaries[0] = []; // outer boundary
-// boundaries[0].push(new WorldWind.Position(-10, 130.007, 1e5));
-// boundaries[0].push(new WorldWind.Position(-99.686, 60.007, 1e5));
-// boundaries[0].push(new WorldWind.Position(-102.686, 60.007, 1e5));
+    let position = new WorldWind.Position(lat, long, 100.0);
 
+    let placemark = new WorldWind.Placemark(position, false, padsAttributes);
 
+    placemark.label = "Pad\n" + pad.name;
+    placemark.alwaysOnTop = true;
 
-// // Create the polygon and assign its attributes.
-
-// var polygon = new WorldWind.Polygon(boundaries, null);
-// polygon.altitudeMode = WorldWind.ABSOLUTE;
-// polygon.extrude = true; // extrude the polygon edges to the ground
-
-// var polygonAttributes = new WorldWind.ShapeAttributes(null);
-// polygonAttributes.drawInterior = true;
-// polygonAttributes.drawOutline = true;
-// polygonAttributes.outlineColor = WorldWind.Color.BLUE;
-// polygonAttributes.interiorColor = new WorldWind.Color(0, 1, 1, 0.5);
-// polygonAttributes.drawVerticals = polygon.extrude;
-// polygonAttributes.applyLighting = true;
-// polygon.attributes = polygonAttributes;
-
-// // Create and assign the polygon's highlight attributes.
-// var highlightAttributes = new WorldWind.ShapeAttributes(polygonAttributes);
-// highlightAttributes.outlineColor = WorldWind.Color.RED;
-// highlightAttributes.interiorColor = new WorldWind.Color(1, 1, 1, 0.5);
-// polygon.highlightAttributes = highlightAttributes;
-
-// // Add the polygon to the layer and the layer to the WorldWindow's layer list.
-// polygonsLayer.addRenderable(polygon);
+    padsLayer.addRenderable(placemark);
+}
--- serve.js
+++ serve.js
@@ -3,5 +3,5 @@ const app = ex();
 
 app.use('/public', ex.static('public'));
 
-app.listen(3030);
+app.listen(3040);
 console.log('Check port: 3030 dude🔥');
