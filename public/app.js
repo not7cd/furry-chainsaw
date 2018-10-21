395cecd0eec1b251ead435bcf4603ff3bfc49a31
Marcin Jasiukowicz � <contact@yasiu.pl>
2018-10-21 10:18:13 +0200

    added more info to cards

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-//all staff to create globe
+//all stuff to create globe
 const globe = new WorldWind.WorldWindow("globe");
 
 globe.addLayer(new WorldWind.BMNGOneImageLayer());
@@ -97,24 +97,32 @@ $(function () {
         }, "slow");
     });
 
+    $("[name=sliderDate]").attr("max", (new Date()).getFullYear() + 5);
+    $("[name=sliderDate]").attr("value", (new Date()).getFullYear());
     $("[name=sliderDate]").change(function () {
         var newval = $(this).val();
         $("#missions-header").html('All Missions from ' + newval);
         fetchData('https://launchlibrary.net/1.4.1/launch/' + newval + '-01-01' + '/' + newval + '-12-31?limit=200', processLaunches);
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".navbar__control").offset().top
+        }, "slow");
     });
+    $("[name=sliderDate]").trigger("change");
 
     $('input[name="daterange"]').daterangepicker({
         opens: 'left'
     }, function (start, end, label) {
-        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-        json = 'https://launchlibrary.net/1.4.1/launch/' + start.format('YYYY-MM-DD') + '/' + end.format('YYYY-MM-DD');
-        console.log(json);
         $("#missions-header").html('All Missions from ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-        fetchData(json, processLaunches);
+        fetchData('https://launchlibrary.net/1.4.1/launch/' + start.format('YYYY-MM-DD') + '/' + end.format('YYYY-MM-DD'), processLaunches);
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".navbar__control").offset().top
+        }, "slow");
     });
 
     $("#missions-header").html('All Missions from ' + (new Date()).getFullYear());
-    fetchData('https://launchlibrary.net/1.4.1/launch/' + (new Date()).getFullYear() + '-01-01' + '/' + (new Date()).getFullYear() + '-12-31?limit=200&status=1,3,4,5,6,7', processLaunches);
+    //fetchData('https://launchlibrary.net/1.4.1/launch/' + (new Date()).getFullYear() + '-01-01' + '/' + (new Date()).getFullYear() + '-12-31?limit=200&status=1,3,4,5,6,7', processLaunches);
     fetchData('https://launchlibrary.net/1.4.1/pad?limit=200', processPads);
 
 })
@@ -141,7 +149,6 @@ function processLaunches(data) {
         window.clearInterval(window.countdowns[d]);
     }
 
-
     globe.goTo(new WorldWind.Location(data.launches[0].location.pads[0].latitude, data.launches[0].location.pads[0].longitude));
 
     data.launches.reverse().map(launch => createLaunch(launch));
@@ -149,26 +156,21 @@ function processLaunches(data) {
 
 // create pin
 function createLaunch(launch) {
+    console.log(launch);
 
     let position = new WorldWind.Position(launch.location.pads[0].latitude, launch.location.pads[0].longitude, 100);
 
     let item = document.createElement('li');
     item.className = 'navbar__list__item';
 
-    item.id = launch.id;
-
-
-    item.innerHTML = "<h1 class='launch-name'>" + launch.name + "</h1><a id='countdown_" + launch.id + "'></a><div class='description'><h1>Description</h1>" + (launch.missions[0] && launch.missions[0].description || "") + "</a>";
+    item.innerHTML = "<h1 class='launch-name'>" + launch.name + "</h1><a class='countdown' id='countdown_" + launch.id + "'></a> | <a class='date'>" + launch.net + "</a><div class='description'><h1>Description</h1>" + (launch.missions[0] && launch.missions[0].description || "No description provided for this mission.") + "<h1>Company</h1><a href='"+(launch.lsp.wikiURL || "")+"' target='_blank'>"+ (launch.lsp.name  || (launch.missions[0] && launch.missions[0].agencies[0] && launch.missions[0].agencies[0].name) || "Unknown") +"</a><h1>Launchsite</h1><a href='"+launch.location.pads[0].wikiURL+"' target='_blank'>"+launch.location.pads[0].name+"</a><h1>Watch</h1><a href='"+launch.vidURLs[0]+"'>"+ (launch.vidURLs[0] && launch.vidURLs[0].split("//")[1].split("/")[0] || "No video available") +"</a></div>";
+    
     if (launch.rocket.imageURL === 'https://s3.amazonaws.com/launchlibrary/RocketImages/placeholder_1920.png') {
         item.style.backgroundColor = '#111111';
     } else {
         item.style.backgroundImage = 'linear-gradient(rgba(0,0,0,30), rgba(0,0,0,0)),url(' + launch.rocket.imageURL + ')';
     }
 
-    (launch.status == 1 || launch.status == 6) ? Countdown(launch.net, document.querySelector("#countdown_" + launch.id)): $("#countdown_" + launch.id).html(statuses[launch.status].name);
-
-
-    console.log(launch);
     list.appendChild(item);
 
     (launch.status == 1 || launch.status == 6) ? Countdown(launch.net, document.querySelector("#countdown_" + launch.id)): $("#countdown_" + launch.id).html(statuses[launch.status].name);
@@ -178,18 +180,13 @@ function createLaunch(launch) {
             scrollTop: 0
         }, "slow");
         globe.goTo(new WorldWind.Position(launch.location.pads[0].latitude, launch.location.pads[0].longitude));
-        // $('.more-info').html('<div class="data-table"><ul><li><h1>Name</h1><p>' + launch.name + '</p></li><li><h1>Description</h1><p>' + launch.description + '</p></li><li><h1>Name</h1><p>' + launch.name + '</p></li></ul></div>')
     });
 
     //BUG: all launches inherit the color of first launch
     placemarkAttributes.labelAttributes.color = WorldWind.Color[statuses[launch.status].color];
 
     let placemark = new WorldWind.Placemark(position, undefined, placemarkAttributes);
-
-
     placemark.label = launch.name + "\n" + statuses[launch.status].name + "\n" + launch.net;
-
-
     placemarkLayer.addRenderable(placemark);
 }
 
