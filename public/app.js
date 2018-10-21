6270ea77e9a13a2c18e20912b8a0a4974b6a5b01
Norbert Szulc <norbert@not7cd.net>
2018-10-21 12:58:55 +0200

    dd

--- public/app.js
+++ public/app.js
@@ -1,6 +1,8 @@
 "use strict";
 
 //all stuff to create globe
+const apiUrl = "http://morning-beach-99281.herokuapp.com/"
+
 const globe = new WorldWind.WorldWindow("globe");
 
 globe.addLayer(new WorldWind.BMNGOneImageLayer());
@@ -121,7 +123,7 @@ $(function () {
     $("[name=sliderDate]").change(function () {
         var newval = $(this).val();
         $("#missions-header").html('All Missions from ' + newval);
-        fetchData('https://launchlibrary.net/1.4.1/launch/' + newval + '-01-01' + '/' + newval + '-12-31?limit=200', processLaunches);
+        fetchData(apiUrl + 'launch/' + newval + '-01-01' + '/' + newval + '-12-31?limit=200', processLaunches);
 
         $([document.documentElement, document.body]).animate({
             scrollTop: $(".navbar__control").offset().top
@@ -133,7 +135,7 @@ $(function () {
         opens: 'left'
     }, function (start, end, label) {
         $("#missions-header").html('All Missions from ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-        fetchData('https://launchlibrary.net/1.4.1/launch/' + start.format('YYYY-MM-DD') + '/' + end.format('YYYY-MM-DD'), processLaunches);
+        fetchData(apiUrl + '/launch/' + start.format('YYYY-MM-DD') + '/' + end.format('YYYY-MM-DD'), processLaunches);
 
         $([document.documentElement, document.body]).animate({
             scrollTop: $(".navbar__control").offset().top
@@ -141,8 +143,8 @@ $(function () {
     });
 
     $("#missions-header").html('All Missions from ' + (new Date()).getFullYear());
-    //fetchData('https://launchlibrary.net/1.4.1/launch/' + (new Date()).getFullYear() + '-01-01' + '/' + (new Date()).getFullYear() + '-12-31?limit=200&status=1,3,4,5,6,7', processLaunches);
-    fetchData('https://launchlibrary.net/1.4.1/pad?limit=200', processPads);
+    //fetchData(apiUrl + '/launch/' + (new Date()).getFullYear() + '-01-01' + '/' + (new Date()).getFullYear() + '-12-31?limit=200&status=1,3,4,5,6,7', processLaunches);
+    fetchData(apiUrl + '/pad?limit=200', processPads);
 
 });
 
